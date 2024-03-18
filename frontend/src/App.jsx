099fe4bc32dd3fde597101af0e71095e2957b918
Soham Gupta <soham.gupta003@gmail.com>
2024-03-18 23:56:06 +0530

    Added Frontend Files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { Dashboard } from './pages/Dashboard'
+import { SendMoney } from './pages/SendMoney'
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
