921525d7fa100611e6fc757e6b7aa73a1b637d4a
Soham Gupta <soham.gupta003@gmail.com>
2024-05-01 22:12:18 +0530

    ui: GitHub Icon & Logout button UI
    - Added GitHub Button that links to the repo
    - Changed UI of Logout Button

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,14 +1,14 @@
-import { Button } from "./Button";
+import Github from "./Github";
 import { UserDetails } from "./UserDetails";
-import { Navigate, useNavigate } from 'react-router-dom'
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const Appbar = () => {
     const navigate = useNavigate();
 
     const user = UserDetails();
-    if(user.loading) {
-        return ;
-    }
+  if (user.loading) {
+    return;
+  }
 
     if(!user.userDetails) {
         return <Navigate to={"/signin"} />
@@ -18,24 +18,29 @@ export const Appbar = () => {
         <div className="flex flex-col justify-center h-full ml-4">
             Payments App
         </div>
-        <div className="flex">
-            <div className="flex flex-col justify-center h-full mr-4">
+        <div className="flex gap-4">
+          <Github />
+            <div className="flex flex-col justify-center h-full">
                 {/* User First Name 👇 */}
                 Hello, {user.userDetails.user.firstName}
             </div>
-            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1">
                 <div className="flex flex-col justify-center h-full text-xl">
                     {/* Profile Picture 👇 */}
                     {user.userDetails.user.firstName[0].toUpperCase()} 
                 </div>
             </div>
-            <div className="flex justify-center mt-1 mr-2">
-                <Button label={"Logout"}
-                onClick={() => {
-                    localStorage.removeItem("token");
-                    navigate("/signin");
-                }} />
-            </div>
+            <div className="flex justify-center mt-1">
+          <button
+            className="w-full bg-gray-200 hover:border hover:text-red-600 hover:border-red-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+            onClick={() => {
+              localStorage.removeItem("token");
+              navigate("/signin");
+            }}
+          >
+            Logout
+          </button>
         </div>
+      </div>
     </div>
 }
